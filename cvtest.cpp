737bbe442fcd52ae5b24efc52569268f741ba42c
Akshat Bubna <akshatb42@gmail.com>
2015-01-06 19:24:21 +0000

    brightness stuff

--- cvtest.cpp
+++ cvtest.cpp
@@ -22,8 +22,15 @@ int main()
 	cap >> testFrame;
 	Mat testOut;
 	processFrame(testFrame, testOut);
+	double alpha=2.0;
+	int beta=80;
+	//std::cout<<"* Enter the alpha value [1.0-3.0]: ";std::cin>>alpha;
+	//std::cout<<"* Enter the beta value [0-100]: "; std::cin>>beta;
 	Size outSize = Size(testOut.cols, testOut.rows);
+	Size recSize = Size(testFrame.cols, testFrame.rows);
 	VideoWriter outVid("test.avi", CV_FOURCC('M','P','4','2'),10,outSize,true);
+	VideoWriter recVid("rec.avi", CV_FOURCC('M','P','4','2'),10,recSize,true);
+	VideoWriter incVid("inc.avi", CV_FOURCC('M','P','4','2'),10,recSize,true);
 	assert(outVid.isOpened());
 	for (int i = 0; i < 50; ++i) {
 		Mat frame;
@@ -32,6 +39,20 @@ int main()
 		Mat out;
 		processFrame(frame, out);
 		outVid << out;
+		recVid << frame;
+		Mat new_image=Mat::zeros( frame.size(), frame.type() );
+		for( int y = 0; y < frame.rows; y++ )
+		{ 
+			for( int x = 0; x < frame.cols; x++ )
+			{ 
+				for( int c = 0; c < 3; c++ )
+				{
+					new_image.at<Vec3b>(y,x)[c] =
+						saturate_cast<uchar>( alpha*( frame.at<Vec3b>(y,x)[c] ) + beta );
+				}
+			}
+		}
+		incVid << new_image;
 		// 100ms
 		struct timespec tim, tim2;
 		tim.tv_sec = 0;
