b1c4f6bbda54cd9a175365fcf703a99a0bbfe448
Akshat Bubna <akshatb42@gmail.com>
2015-01-12 21:47:50 +0000

    Joe new modified

---
+++ motortest.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include "motor.cpp"
+#include "gyro.cpp"
+
+int running=1;
+
+void sig_handler(int signo)
+{
+    if (signo == SIGINT) {
+        printf("closing PWM nicely\n");
+        running = 0;
+    }
+};
+
+int main()
+{
+	signal(SIGINT, sig_handler);
+	Motor left(8,6,4,false),right(9,5,2,true);
+	Gyroscope gyr(10);
+
+	const float target=0, K=0.003, base=0.15;
+
+	left.forward();
+	right.forward();
+
+	left.setSpeed(base);
+	right.setSpeed(base);
+
+	
+	while(running) 
+	{
+		/*
+		float angle=gyr.run();
+		float diff=(angle-target)*K;
+		std::cout<<angle<<" "<<diff<<std::endl;
+		left.setSpeed(base);
+		right.setSpeed(base);
+		if(diff>0) left.setSpeed(base+diff);
+		else right.setSpeed(base-diff);
+		sleep(0.01);
+		*/
+		float realspeed = right.rps();
+		std::cout<<realspeed<<" "<<std::endl;
+	}
+
+	left.setSpeed(0);
+	right.setSpeed(0);
+    	sleep(1);
+    	return 0;
+}
