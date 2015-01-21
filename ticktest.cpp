2724812b92ca6965d041827d67b592b9df0b10af
Akshat Bubna <akshatb42@gmail.com>
2015-01-21 21:57:36 +0000

    picking up blocks

--- ticktest.cpp
+++ ticktest.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "motion.cpp"
+#include "servo.cpp"
 
 int running=1;
 
@@ -14,13 +15,57 @@ void sig_handler(int signo)
 int main()
 {
 	signal(SIGINT, sig_handler);
+	Motor* base = new Motor(10,11,6,false); //base motor -counterclockwise when forward
+	Motor* lift = new Motor(8,9,6,false); //lift motor
 	Motor* left = new Motor(0,2,4,false);
 	Motor* right = new Motor(4,6,2,true);
 	Odometry* odo = new Odometry(left, right, 0, 0, 0);
 	Location* location = new Location(0.0,0.0,0.0);
 
-	//motion->rotate(1.57);	
-	left->turnAngle(360,0.5);
+	Motion* motion = new Motion(left,right,odo,location); 
+
+	motion->straight(1);
+	while(running&&!motion->run() ) usleep(10000);
+	left->stop();
+	right->stop();
+	sleep(1);
+
+	Servo claw(15);
+
+	claw.write(0.7);
+
+	lift->forward();
+	lift->setSpeed(5);
+	usleep(1700000);
+	lift->stop();
+	sleep(1);
+
+	claw.write(0.15);
+	sleep(1);
+
+	lift->backward();
+	lift->setSpeed(5);
+	usleep(1700000);
+	lift->stop();
+	sleep(1);
+
+	base->forward();
+	base->turnAngle(90,3);
+
+	lift->forward();
+	lift->setSpeed(5);
+	usleep(100000);
+	lift->stop();
+
+	claw.write(0.7);
+	sleep(1);
+
+	lift->backward();
+	lift->setSpeed(5);
+	usleep(100000);
+	lift->stop();
+
+	claw.release();
 	sleep(1);
 	return 0;
 }
