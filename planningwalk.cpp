45bca1dddeb6c235676e142424d381412829fa5b
Akshat Bubna <akshatb42@gmail.com>
2015-01-22 22:15:23 +0000

    planningwalk

--- planningwalk.cpp
+++ planningwalk.cpp
@@ -1,12 +1,10 @@
 #include <iostream>
 #include "shortIR.cpp"
-#include "Motor.cpp"
-#include "Odometry.cpp"
-#include "data.cpp"
+#include "odometry.cpp"
 const float slp = 0.5;
 const float forward_speed = 1.2;
 const float forward_side_dis_threshold = 12;
-const float forward_front_dis_threshold = 8;
+const float forward_front_dis_threshold = 10;
 const float block_size = 3.5;
 
 class Planningwalk {
@@ -25,7 +23,7 @@ class Planningwalk {
 
 
 public:
-	Planningwalk (Motor* _left,Motor* _right,IR* _irf,IR* _irlf,IR* _irlb,IR* _irr,mraa::Gpio* _irb, Laction* _start) {
+	Planningwalk (Motor* _left,Motor* _right,IR* _irf,IR* _irlf,IR* _irlb,IR* _irr,mraa::Gpio* _irb, Location* _start) {
 		left = _left; right = _right;
 		irf = _irf; irlf = _irlf; irlb = _irlb; irr = _irr; irb = _irb;
 		start = _start; current = _start;
@@ -58,17 +56,18 @@ public:
 		if (wall_side==1) {
 			std::cout<<"wall in right: "<<irr->getDistance()<<std::endl;
 		}
-		if (wall_side==0) {
+		if (wall_side==2) {
 			std::cout<<"wall in front: "<<irf->getDistance()<<std::endl;
 		}
+	}
 	bool forward_next () {
 		float dlf = irlf->getDistance();
 		float dr = irr->getDistance();
-		float df = iif->getDistance();
+		float df = irf->getDistance();
 		std::cout<<"dlf: "<< dlf<<" dr: "<<dr<<" df: "<<df<<std::endl;
 		if (!((dlf>forward_side_dis_threshold) && (dr>forward_side_dis_threshold) && (df>forward_front_dis_threshold) && (!irb->read()))) {
 			if (df<=forward_front_dis_threshold) {   //front wall dealer
-				if (f_cnt<3) f_cnt++;
+				if (f_cnt<0) f_cnt++;
 				else {
 					wall_side = 2;
 					return true;
