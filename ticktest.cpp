82e04af6c7609590432ff0291aa71ff52a4ab0c1
Akshat Bubna <akshatb42@gmail.com>
2015-01-23 04:24:40 +0000

    lifitng and things

--- ticktest.cpp
+++ ticktest.cpp
@@ -16,7 +16,7 @@ int main()
 {
 	signal(SIGINT, sig_handler);
 	Motor* base = new Motor(10,11,6,false); //base motor -counterclockwise when forward
-	Motor* lift = new Motor(8,9,6,false); //lift motor
+	Motor* lift = new Motor(8,9,3,false); //lift motor
 	Motor* left = new Motor(0,2,4,false);
 	Motor* right = new Motor(4,6,2,true);
 	Odometry* odo = new Odometry(left, right, 0, 0, 0);
@@ -24,46 +24,44 @@ int main()
 
 	Motion* motion = new Motion(left,right,odo,location); 
 
+
+
 	//motion->straight(12);
 	//while(running&&!motion->run() ) usleep(10000);
-	left->stop();
-	right->stop();
-	sleep(1);
+	//left->stop();
+	//right->stop();
+	//sleep(1);
 
 	Servo claw(15);
 
 	claw.write(0.7);
 
 	lift->forward();
-	lift->setSpeed(5);
-	usleep(1700000);
-	lift->stop();
-	sleep(1);
+
+	lift->turnAngle(1930,3);
 
 	claw.write(0.15);
 	sleep(1);
 
 	lift->backward();
-	lift->setSpeed(5);
-	usleep(1700000);
-	lift->stop();
+	lift->turnAngle(1930,3);
+
 	sleep(1);
 
 	base->forward();
 	base->turnAngle(90,3);
 
+	sleep(1);
+
 	lift->forward();
-	lift->setSpeed(5);
-	usleep(100000);
-	lift->stop();
+	lift->turnAngle(100,3);
 
 	claw.write(0.7);
 	sleep(1);
 
+
 	lift->backward();
-	lift->setSpeed(5);
-	usleep(100000);
-	lift->stop();
+	lift->turnAngle(100,3);
 
 	claw.release();
 	sleep(1);
