c61da98d8ba4dbb033e29a47cfd1d05ff5d96328
Mitchell Gu <mitchgu@mitchgu.me>
2015-01-28 00:40:41 -0500

    roomba base

---
+++ roomba.cpp
@@ -0,0 +1,46 @@
+#include "motion.cpp"
+#include <iostream>
+#include "shortIR.cpp"
+
+class Roomba {
+  IR* irlf;
+  IR* irlb;
+  IR* irr;
+  IR* irf;
+  mraa::Gpio* uirb;
+
+  Motor* left;
+  Motor* right;
+  Odometry* odo;
+  Location* start;
+  Location* current;
+  Motion* motion;
+
+public:
+  Roomba(Motor* _l, Motor* _r, IR* _irf, IR* _irr, IR* _irlf, IR* _irlb, mraa::Gpio* _uirb, Location* _start) {
+    left = _l;
+    right = _r;
+    irlf = _irlf;
+    irlb = _irlb;
+    irr = _irr;
+    irf = _irf;
+    uirb = _uirb;
+
+    current = _start;
+    start=new Location(current);
+    //odo = new Odometry(_l, _r, _start->x(),_start->y(),_start->theta());
+    odo = new Odometry(_l, _r, current);
+    motion = new Motion(left,right,odo,_start);
+  }
+
+  int step(int state) {
+    if (state == 0) {
+      std::cout<<"Front: "<<irf->getDistance()<std::endl;
+      std::cout<<"Right: "<<irf->getDistance()<std::endl;
+      std::cout<<"Left-front: "<<irlf->getDistance()<std::endl;
+      std::cout<<"Left-back: "<<irlb->getDistance()<std::endl;
+    }
+    return 0;
+  }
+
+}
---
+++ roombatest.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include "roomba.cpp"
+
+int running=1;
+void sig_handler(int signo)
+{
+    if (signo == SIGINT) {
+        printf("closing PWM nicely\n");
+        running = 0;
+    }
+};
+
+int main(){
+  signal(SIGINT, sig_handler);
+  Motor* left = new Motor(0,2,4,false);
+  Motor* right = new Motor(4,6,2,true);
+  Location* location = new Location(0.0,0.0,0.0);
+  IR* irf = new IR(0);
+  IR* irr = new IR(1);
+  IR* irlf = new IR(3);
+  IR* irlb = new IR(2);
+  mraa::Gpio* uirb = new mraa::Gpio(8);
+  Roomba* roomba= new Roomba(left,right,irf,irr,irlf,irlb,uirb,location);
+  int state=0;
+  //bool localized = false;
+  while(running) {// &&!localized) {
+    state = roomba->step(state);
+    //int mode=wf->locating_channel();
+  } 
+  left->stop();
+  right->stop();
+  sleep(1); 
+  return 0;
+}
+
